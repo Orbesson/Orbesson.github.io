ed89e52e9ea32855c3f087209876874e7ff9e5be
Orbesson <05thmab@skola.vara.se>
2023-05-21 23:26:30 +0200

    Final update.
    The day the music died. Bye world.

--- schlattahotellet/js/script.js
+++ schlattahotellet/js/script.js
@@ -1,10 +1,17 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-5rem";
-  }
-  prevScrollpos = currentScrollPos;
+if(screen.width < 720) { 
+  var adfly_id = null; 
+  var popunder_frequency_delay = null;
+} else {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-5rem";
+    }
+    prevScrollpos = currentScrollPos;
+  }    
+  var adfly_id = ID; 
+  var popunder_frequency_delay = 0;
 }
